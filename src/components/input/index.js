662f635ceba7123ef8c92bc9d6aa070f72fa3c1a
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2020-11-30 18:15:53 +0100

    input modifications

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./styles.css";
 
 const Input = ({ placeholder }) => (
- <input type="text" placeholder={placeholder} className="placeholder-component"></input>
+ <input type="text" placeholder={placeholder} className="input-component"></input>
 );
 
 export default Input;
