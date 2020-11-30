0e5b9dae307cf048c272541f914d3f786e048ad3
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2020-11-30 18:03:06 +0100

    header

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import "./styles.css";
 
-const Button = (title, linkTo, size) => (
+const Button = ({ title, linkTo, size }) => (
  <a href={linkTo} className={`button-component button-${size}`}>
   {title}
  </a>
