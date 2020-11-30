a83e6520b3d37e5832d44954430d89203333ebd2
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2020-11-30 19:12:21 +0100

    home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./styles.css";
-import button from "../../components/button"
 import Button from '../../components/button';
+import Header from '../../components/header';
 
 const Home = () => (
  <section className="home-section">
+  <Header title="Home" backTo="" />
   <Button title="Take care of my body" linkTo="/bodyMenu" size="big" />
   <Button title="Take care of my mind" linkTo="/mindMenu" size="big" />
   <Button title="Have fun" linkTo="/haveFun" size="big" />
