3afdcde7fe1d49ccb3de23fe695daa2fb1486723
carlos-ch <carlos.tschavez@gmail.com>
2020-12-01 13:19:57 +0100

    removed untracked file, unused lines in config

--- src/config/meditation.js
+++ src/config/meditation.js
@@ -3,23 +3,19 @@ const guides = {
     {
       name: 'guide1',
       duration: '3 minutes',
-      source: '../../media/guide1.mp3',
     },
     {
       name: 'guide2',
       duration: '5 minutes',
-      source: '../../media/guide2.mp3',
     },
     {
       name: 'guide3',
 
       duration: '10 minutes',
-      source: '../../media/guide3.mp3',
     },
     {
       name: 'awarenessGuide',
       duration: 'Awareness',
-      source: '../../media/awareness.mp3',
     },
   ],
 };
